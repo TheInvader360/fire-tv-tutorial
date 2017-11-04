4ebf939c56d3e89a3b07330479e0923f4945bd1d
TheInvader360 <theinvader360@gmail.com>
2017-11-04 08:51:00 +0000

    new libgdx project

---
+++ desktop/src/com/theinvader360/tutorial/fire/tv/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.theinvader360.tutorial.fire.tv.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.theinvader360.tutorial.fire.tv.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
